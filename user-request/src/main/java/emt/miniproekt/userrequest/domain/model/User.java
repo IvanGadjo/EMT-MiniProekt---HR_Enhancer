abdca02a43765eb77897ed8c721f1650f64721f1
Andrej Hristov <andrejhris23@gmail.com>
2020-06-21 18:39:48 +0200

    added new user class

---
+++ user-request/src/main/java/emt/miniproekt/userrequest/domain/model/User.java
@@ -0,0 +1,48 @@
+package emt.miniproekt.userrequest.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+
+public class User {
+
+    @Version
+    private Long version;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @SuppressWarnings("unused")
+    private User() {
+
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
