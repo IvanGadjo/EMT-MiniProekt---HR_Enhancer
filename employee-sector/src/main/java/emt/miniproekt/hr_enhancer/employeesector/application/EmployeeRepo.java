3cd03467e3c5be3fa2ee2f369c08e5a5da10d121
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2020-06-22 21:05:56 +0200

    employee and sector repos

--- employee-sector/src/main/java/emt/miniproekt/hr_enhancer/employeesector/application/EmployeeRepo.java
+++ employee-sector/src/main/java/emt/miniproekt/hr_enhancer/employeesector/application/EmployeeRepo.java
@@ -1,11 +1,36 @@
 package emt.miniproekt.hr_enhancer.employeesector.application;
 
+import emt.miniproekt.hr_enhancer.employeesector.domain.model.Employee;
+import emt.miniproekt.hr_enhancer.employeesector.domain.repository.EmployeeRepoJPA;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class EmployeeRepo {
 
+    EmployeeRepoJPA employeeRepoJPA;
+
+    public EmployeeRepo(EmployeeRepoJPA employeeRepoJPA){
+        this.employeeRepoJPA = employeeRepoJPA;
+    }
+
+    public List<Employee> findAll(){
+        return employeeRepoJPA.findAll();
+    }
+
+    public Employee findById(int id){
+        return employeeRepoJPA.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    public Employee saveNewEmployee(Employee employee){
+        return employeeRepoJPA.save(employee);
+    }
+
+    public void deleteById(int id){
+        employeeRepoJPA.deleteById(id);
+    }
 
 }
---
+++ employee-sector/src/main/java/emt/miniproekt/hr_enhancer/employeesector/application/SectorRepo.java
@@ -0,0 +1,37 @@
+package emt.miniproekt.hr_enhancer.employeesector.application;
+
+
+import emt.miniproekt.hr_enhancer.employeesector.domain.model.Employee;
+import emt.miniproekt.hr_enhancer.employeesector.domain.model.Sector;
+import emt.miniproekt.hr_enhancer.employeesector.domain.repository.SectorRepoJPA;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class SectorRepo {
+
+    SectorRepoJPA sectorRepoJPA;
+
+    public SectorRepo(SectorRepoJPA sectorRepoJPA){
+        this.sectorRepoJPA = sectorRepoJPA;
+    }
+
+    public List<Sector> findAll(){
+        return sectorRepoJPA.findAll();
+    }
+
+    public Sector findById(int id){
+        return sectorRepoJPA.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    public Sector saveNewEmployee(Sector sector){
+        return sectorRepoJPA.save(sector);
+    }
+
+    public void deleteById(int id){
+        sectorRepoJPA.deleteById(id);
+    }
+}
