84492564b8283c90cc9601914cdd0a6b5dc71a5b
Andrej Hristov <andrejhris23@gmail.com>
2020-06-28 19:21:00 +0200

    added all postmapping methods in UserRequestsAPI

--- user-request/src/main/java/emt/miniproekt/userrequest/rest/UserRequestsAPI.java
+++ user-request/src/main/java/emt/miniproekt/userrequest/rest/UserRequestsAPI.java
@@ -1,7 +1,6 @@
 package emt.miniproekt.userrequest.rest;
 
-import emt.miniproekt.userrequest.domain.model.ComplaintRequest;
-import emt.miniproekt.userrequest.domain.model.User;
+import emt.miniproekt.userrequest.domain.model.*;
 import emt.miniproekt.userrequest.domain.service.UsersService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,13 +20,37 @@ public class UserRequestsAPI {
     @PostMapping("/complaintRequest")
     public ComplaintRequest makeNewComplaintReq(@RequestParam("employeeId") int employeeId,
                                                 @RequestParam("description") String description,
-                                                @RequestParam("userId") int userId){
+                                                @RequestParam("userId") int userId) {
         return usersService.makeComplaintRequest(employeeId, description, userId);
+    }
+
+    @PostMapping("/positionChangeRequest")
+    public PositionChangeRequest makeNewPositionChangeReq(@RequestParam("employeeId") int employeeId,
+                                                          @RequestParam("description") String description,
+                                                          @RequestParam("newPosition") String newPosition,
+                                                          @RequestParam("userId") int userId) {
+        return usersService.makePositionChangeRequest(employeeId, description, newPosition, userId);
+    }
+
+    @PostMapping("/raiseRequest")
+    public RaiseRequest makeNewRaiseReq(@RequestParam("employeeId") int employeeId,
+                                        @RequestParam("description") String description,
+                                        @RequestParam("newSalary") double newSalary,
+                                        @RequestParam("userId") int userId) {
+        return usersService.makeRaiseRequest(employeeId, description, newSalary, userId);
+    }
+
+    @PostMapping("/restDaysRequest")
+    public RestDaysRequest makeNewRestDaysReq(@RequestParam("employeeId") int employeeId,
+                                              @RequestParam("description") String description,
+                                              @RequestParam("numDays") int numDays,
+                                              @RequestParam("userId") int userId) {
+        return usersService.makeRestDaysRequest(employeeId, description, numDays, userId);
     }
 
     @PostMapping("/makeNewUser")
     public User addUser(@RequestParam("username") String username,
-                        @RequestParam("password") String password){
+                        @RequestParam("password") String password) {
         return usersService.makeNewUser(username, password);
     }
 
